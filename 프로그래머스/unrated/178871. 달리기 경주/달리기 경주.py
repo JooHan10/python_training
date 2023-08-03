7c1ef12c4755f7b041879c8f3fdff569002bf1a0
JooHan10 <116674496+JooHan10@users.noreply.github.com>
2023-08-03 10:34:29 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,15 +1,12 @@
 def solution(players, callings):
-    answer = []
-    dic1={ player:idx for idx,player in enumerate(players,1)}
-    dic2={ idx:player for idx,player in enumerate(players,1)}
+    player_dict = {player: idx for idx, player in enumerate(players)}
     for calling in callings:
-        rank = dic1[calling]
-        pre_rank = rank-1
-        tmp = dic2[pre_rank]
-        dic2[pre_rank]=dic2[rank]
-        dic2[rank]=tmp
-        dic1[dic2[rank]] = rank
-        dic1[dic2[pre_rank]] = pre_rank
-    for rank in dic2.values():
-        answer.append(rank)
-    return answer
+        current_idx = player_dict[calling]
+        # print(current_idx)
+        player_dict[calling] -= 1
+        # print(player_dict[calling])
+        player_dict[players[current_idx-1]] += 1
+        # print(player_dict[players[current_idx-1]])
+        players[current_idx-1], players[current_idx] = players[current_idx], players[current_idx-1]
+        
+    return players
