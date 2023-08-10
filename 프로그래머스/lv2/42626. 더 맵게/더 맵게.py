c39c01a1ce5d81202f8feac48485c9ed8fe34c94
JooHan10 <116674496+JooHan10@users.noreply.github.com>
2023-08-10 23:18:59 +0900

    [level 2] Title: 더 맵게, Time: 1645.95 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(scoville, K):
+    scoville_heap = []
+    for sco in scoville:
+        heapq.heappush(scoville_heap, sco)
+
+    cnt = 0
+    while scoville_heap[0] < K:
+        heapq.heappush(scoville_heap, heapq.heappop(scoville_heap) + heapq.heappop(scoville_heap) * 2)
+        cnt += 1
+        
+        if len(scoville_heap) == 1 and scoville_heap[0] < K:
+            return -1
+    return cnt
